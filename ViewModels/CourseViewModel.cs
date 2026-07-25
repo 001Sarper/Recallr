217689b6efbc960f38516b8b86af80ad8a42b878
dipsy.03 <dipsytech@proton.me>
2026-07-25 13:07:38 +0200

    feat: Course edit & deletion feature implemented

--- ViewModels/CourseViewModel.cs
+++ ViewModels/CourseViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -12,17 +13,28 @@ namespace Recallr.ViewModels;
 public partial class CourseViewModel : ViewModelBase
 {
     public CoursesService CoursesService => CoursesService.Instance;
-
+    public OverlayService OverlayService => OverlayService.Instance;
+    
+    private static string _coursesConfigPath;
+    private ConfigManager _configManager;
+    
+    
     public CourseViewModel()
     {
-        CoursesService.Instance.LoadCourses();
+        CoursesService.LoadCourses();
+        _coursesConfigPath =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Recallr", "Config", "ClientCourses.json");
+        
+        var json = File.ReadAllText(_coursesConfigPath);
+        _configManager = JsonSerializer.Deserialize<ConfigManager>(json) ?? new ConfigManager();
     }
     
     [RelayCommand]
     public void ShowOverlay()
     {
-        OverlayService.Instance.IsOverlayVisible = true;
-        OverlayService.Instance.ShowNewCourseOverlayCommand.Execute(null);
+        OverlayService.ButtonContent = "Erstellen";
+        OverlayService.IsOverlayVisible = true;
+        OverlayService.ShowOverlay();
     }
     
     
@@ -35,13 +47,35 @@ public partial class CourseViewModel : ViewModelBase
     [RelayCommand]
     private void EditCourse(ClientCourses course)
     {
-        // z.B. das gleiche Overlay wie beim Erstellen öffnen, 
-        // aber mit vorausgefüllten Werten aus `course`
+        
+        OverlayService.CourseEmoji = course.Icon;
+        OverlayService.CourseName = course.Name;
+        OverlayService.CourseTeacher = course.TeacherName;
+        OverlayService.course = course;
+        OverlayService.ButtonContent = "Bearbeiten";
+        OverlayService.IsOverlayEditMode = true;
+        OverlayService.ShowOverlay();
     }
 
     [RelayCommand]
     private void DeleteCourse(ClientCourses course)
     {
+        var configEntry = _configManager.ClientCourses
+            .FirstOrDefault(c => c.ID == course.ID);
+    
+        if (configEntry != null)
+            _configManager.ClientCourses.Remove(configEntry);
+
+        CoursesService.Courses.Remove(course);
         
+        SaveConfig();
+        
+    }
+    
+    private void SaveConfig()
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        var json = JsonSerializer.Serialize(_configManager, options);
+        File.WriteAllText(_coursesConfigPath, json);
     }
 }
